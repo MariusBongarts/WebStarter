052d85db5e756cde5cd7888f511bce3f36f45837
Marius Bongarts <bongarts@westphalia-datalab.com>
2019-09-27 13:03:45 +0200

    Adjust starter

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,6 +1,4 @@
-import { Mark } from './../models/mark';
 import { css, customElement, html, LitElement, property, unsafeCSS, query } from 'lit-element';
-import { MarkerService } from '../services/marker.service';
 
 const componentCSS = require('./app.component.scss');
 
@@ -10,33 +8,20 @@ const componentCSS = require('./app.component.scss');
  */
 @customElement('app-root')
 export class AppRoot extends LitElement {
-  markService = new MarkerService();
 
   static styles = css`${unsafeCSS(componentCSS)}`;
-
-  @property() marks!: Mark[];
 
   @property()
   title: string = 'LitElement Starter';
 
   async firstUpdated() {
-    this.marks = await this.markService.getMarks();
   }
 
   render() {
     return html`
     <h1>${this.title}</h1>
-          ${this.marks && this.marks.length ? this.marks.map(mark => html`
-          <div class="entry">
-          <h5>${mark.origin}</h5><br><span>${this.getDateString(mark.createdAt)}: ${mark.text}</span>`) : html`<h1>No marks made yet</h1>
-          </div>
-          `}
+    <slot></slot>
 `;
-  }
-
-  getDateString(date: number) {
-    const newDate = new Date(date);
-    return `${newDate.getHours()}:${newDate.getMinutes()} Uhr`;
   }
 
 
--- src/models/mark.ts
+++
@@ -1,8 +0,0 @@
-export interface Mark {
-  url: string,
-  origin: string,
-  text: string
-  anchorNodeText: string,
-  anchorOffset: number,
-  createdAt: number
-}
